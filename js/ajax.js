5fc1cc412b1f99bd0ee89336755fa9a22845a845
Wence Criado <wence74@gmail.com>
2017-07-16 11:04:27 +0200

    "Subo todo el proyecto"

---
+++ js/ajax.js
@@ -0,0 +1,93 @@
+var tasks = [];
+
+var drawTasks = function() {
+    $('#taskContainer').empty();
+
+    if (tasks.length == 0) {
+        $('#taskContainer').append("<tr><td>No hay tareas pendientes</td></tr>");
+    } else {
+        var contentToAdd = '';
+        for (var i = 0; i < tasks.length; i++) {
+            contentToAdd += '<tr><td class="task-item">' + tasks[i].name + '<button class="deleteTask" ' +
+                'data-task-id="' + tasks[i].id + '">Eliminar</button></td></tr>'
+        }
+        $('#taskContainer').append(contentToAdd);
+    }
+}
+
+var getTasks = function () {
+    var XHR = new XMLHttpRequest();
+    XHR.open("GET", "http://localhost:8000/api/tasks", true);
+    XHR.setRequestHeader("Content-Type", "application/json");
+
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState === 4) {
+            tasks = JSON.parse(XHR.responseText);
+            drawTasks();
+        } else if (XHR.readyState === 4 && XHR.status === 404) {
+            console.log("Página no encontrada");
+        }
+    }
+
+    XHR.send();
+}
+
+var createTask = function (name) {
+    var XHR = new XMLHttpRequest();
+    XHR.open("POST", "http://localhost:8000/api/tasks", true);
+    XHR.setRequestHeader("Content-Type", "application/json");
+
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState === 4) {
+            tasks.push(JSON.parse(XHR.responseText));
+            drawTasks();
+        } else if (XHR.readyState === 4 && XHR.status === 404) {
+            console.log("Página no encontrada");
+        }
+    }
+
+    XHR.send(JSON.stringify({"name": name}));
+}
+
+var deleteTask = function (id) {
+    var XHR = new XMLHttpRequest();
+    XHR.open("DELETE", "http://localhost:8000/api/tasks/" + id, true);
+    XHR.setRequestHeader("Content-Type", "application/json");
+
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState === 4) {
+            console.log("tasks deleted");
+            getTasks();
+        } else if (XHR.readyState === 4 && XHR.status === 404) {
+            console.log("Página no encontrada");
+        }
+    }
+
+    XHR.send();
+}
+var elemento = document.getElementById("newTaskName");
+document.getElementById("sendNewTask").addEventListener("click", function (event) {
+    if(elemento.value == "") {
+        var XHR = new XMLHttpRequest();
+        $("#errorAjax").show();
+        XHR.abort();
+    }
+
+        else{
+        $("#errorAjax").hide();
+            event.preventDefault();
+            createTask(document.getElementById("newTaskName").value);
+        }
+
+
+})
+
+
+getTasks();
+
+$(document).on('click', '.deleteTask', function(){
+    var id = $(this).data('taskId');
+    deleteTask(id);
+});
+
+
---
+++ js/form.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Wence on 9/7/17.
+ */
+var form = document.getElementsByTagName('form')[0];
+console.log(form);
+
+var submitButton = document.getElementById('Enviar');
+
+
+var loadingIcon =  document.createElement('i');
+loadingIcon.classList.add("fa", "fa-spinner", "fa-spin");
+
+form.addEventListener('submit', function (event) {
+
+    submitButton.setAttribute("disabled","");
+    submitButton.appendChild(loadingIcon);
+    event.preventDefault();
+
+
+    setTimeout(function () {
+        submitButton.removeAttribute("disabled");
+        submitButton.removeChild(loadingIcon);
+        $('#contacto-recibido').show().addClass('contacto-animacion');
+    }, 1000);
+});
